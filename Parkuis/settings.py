4419f71e33b3fe7983213da278da7b96270aa8f9
Minerisho <migueplata5@gmail.com>
2024-05-01 12:15:25 -0500

    se añadió la app vehiculos

--- Parkuis/settings.py
+++ Parkuis/settings.py
@@ -103,13 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CO'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+DEFAULT_CHARSET = 'utf-8'
 
 
 # Static files (CSS, JavaScript, Images)
--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path('login', views.login),
-    re_path('signup', views.signup),
-    re_path('test_token', views.test_token),
-    re_path('change_pass', views.change_pass),
-    re_path('logout', views.logout)
+    re_path('login', views.login, name='login'),
+    re_path('signup', views.signup, name='registro'),
+    re_path('test_token', views.test_token, name='test token'),
+    re_path('change_pass', views.change_pass, name='cambiar contra'),
+    re_path('logout', views.logout, name='logout')
 ]
